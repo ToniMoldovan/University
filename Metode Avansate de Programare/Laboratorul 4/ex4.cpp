7ec03ff6b811bb12939f5ffa7e2ff761057c597e
ToniMoldovan <45207785+ToniMoldovan@users.noreply.github.com>
2021-02-26 21:03:52 +0200

    Add files via upload

---
+++ Metode Avansate de Programare/Laboratorul 4/ex4.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <stdarg.h>
+#include <algorithm>
+using namespace std;
+
+void afisare_numere(int n, ...);
+
+int main()
+{
+    afisare_numere(5, 3, 4, 8, 1, 9);
+
+    system("pause");
+    return 0;
+}
+
+void afisare_numere(int n, ...)
+{
+    va_list argumente;
+    va_start(argumente, n);
+    int vector[50];
+    for (int i = 0; i < n; i++)
+    {
+        vector[i] = va_arg(argumente, int);
+    }
+    //Afisare vector fara sortare
+    cout << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << vector[i] << " ";
+    }
+
+    //Sortare vector (bubble sort)
+    int sortat;
+    do
+    {
+        sortat = 1;
+        for (int i = 0; i < n; i++)
+        {
+            if (vector[i] > vector[i + 1])
+            {
+                int aux = vector[i];
+                vector[i] = vector[i + 1];
+                vector[i + 1] = aux;
+                sortat = 0;
+            }
+        }
+    } while (sortat == 0);
+
+    cout << endl;
+
+    //Afisare vector sortat
+    for (int i = 0; i < n; i++)
+    {
+        cout << vector[i] << " ";
+    }
+
+    va_end(argumente);
+}
